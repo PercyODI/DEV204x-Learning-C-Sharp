c29db162f721bba55496d94e7b5bea21319c0c2b
Pearse Hutson <pearse.2008@gmail.com>
2016-09-22 20:36:36 -0500

    ModuleTwo Assignment

--- ModuleTwoAssignment/ModuleTwoAssignment/Program.cs
+++ ModuleTwoAssignment/ModuleTwoAssignment/Program.cs
@@ -10,6 +10,62 @@ namespace ModuleTwoAssignment
     {
         static void Main(string[] args)
         {
+            Boolean again = false;
+            do
+            {
+                Console.Write("Enter Number of Rows: ");
+                int numRows = Convert.ToUInt16(Console.ReadLine());
+
+                Console.Write("Enter Number of Columns: ");
+                int numCols = Convert.ToUInt16(Console.ReadLine());
+
+                Console.Write("\n");
+
+                for (int rowCounter = 0; rowCounter < numRows; rowCounter++)
+                {
+                    if (rowCounter % 2 == 0)
+                    {
+                        for (int colCounter = 0; colCounter < numCols; colCounter++)
+                        {
+                            if (colCounter % 2 == 0)
+                            {
+                                Console.Write("X");
+                            }
+                            else
+                            {
+                                Console.Write("O");
+                            }
+                        }
+                        Console.Write("\n");
+                    }
+                    else
+                    {
+                        for (int colCounter = 0; colCounter < numCols; colCounter++)
+                        {
+                            if (colCounter % 2 == 0)
+                            {
+                                Console.Write("O");
+                            }
+                            else
+                            {
+                                Console.Write("X");
+                            }
+                        }
+                        Console.Write("\n");
+                    }
+                }
+
+                Console.Write("Would you like to make another (y or n)? ");
+                String userResponse = Console.ReadLine();
+                if (userResponse == "y" || userResponse == "Y")
+                {
+                    again = true;
+                }
+                else
+                {
+                    again = false;
+                }
+            } while (again == true);
         }
     }
 }
